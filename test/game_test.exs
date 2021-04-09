6d334df7f6b5866c65292c9168a3d86c66a308e2
kretzer <fabiolakretzer@gmail.com>
2021-04-09 18:39:56 -0300

    test: create and refactor tests for functions of the game module

--- test/game_test.exs
+++ test/game_test.exs
@@ -3,109 +3,106 @@ defmodule ExMon.GameTest do
 
   alias ExMon.{Game, Player}
 
+  @player ExMon.create_pĺayer("Jogador1", :chute, :soco, :cura)
+  @computer ExMon.create_pĺayer("Robotinik", :chute, :soco, :cura)
+  @life 50
+  @start_game_computer %{
+      computer: %Player{
+        life: 100,
+        moves: %{
+          move_avg: :chute,
+          move_heal: :cura,
+          move_rnd: :soco
+          },
+        name: "Robotinik"
+      },
+      player: %Player{
+        life: 100,
+        moves: %{
+          move_avg: :chute,
+          move_heal: :cura,
+          move_rnd: :soco
+        },
+        name: "Jogador1"
+      },
+      status: :started,
+      turn: :computer
+    }
+
+  @start_game_player %{@start_game_computer | turn: :player}
+
   describe "start/2" do
     test "starts the game state" do
-      player = ExMon.create_pĺayer("Jogador1", :chute, :soco, :cura)
-      computer = ExMon.create_pĺayer("Robotinik", :chute, :soco, :cura)
-
-      assert {:ok, _pid} = Game.start(computer, player, :computer)
+      assert {:ok, _pid} = Game.start(@computer, @player, :computer)
     end
   end
 
   describe "info/0" do
     test "returns the current game state" do
-      player = ExMon.create_pĺayer("Jogador1", :chute, :soco, :cura)
-      computer = ExMon.create_pĺayer("Robotinik", :chute, :soco, :cura)
+      Game.start(@computer, @player, :computer)
 
-      Game.start(computer, player, :computer)
-
-      expected_response = %{
-        computer: %Player{
-          life: 100,
-          moves: %{
-            move_avg: :chute,
-            move_heal: :cura,
-            move_rnd: :soco
-            },
-          name: "Robotinik"
-        },
-        player: %Player{
-          life: 100,
-          moves: %{
-            move_avg: :chute,
-            move_heal: :cura,
-            move_rnd: :soco
-          },
-          name: "Jogador1"
-        },
-        status: :started,
-        turn: :computer
-      }
+      expected_response = @start_game_computer
 
       assert expected_response == Game.info()
     end
   end
 
   describe "update/1" do
-    test "returns the game sate updated" do
-      player = ExMon.create_pĺayer("Jogador1", :chute, :soco, :cura)
-      computer = ExMon.create_pĺayer("Robotinik", :chute, :soco, :cura)
+    test "returns the game state updated" do
+      Game.start(@computer, @player, :player)
 
-      Game.start(computer, player, :player)
-
-      expected_response = %{
-        computer: %Player{
-          life: 100,
-          moves: %{
-            move_avg: :chute,
-            move_heal: :cura,
-            move_rnd: :soco
-            },
-          name: "Robotinik"
-        },
-        player: %Player{
-          life: 100,
-          moves: %{
-            move_avg: :chute,
-            move_heal: :cura,
-            move_rnd: :soco
-          },
-          name: "Jogador1"
-        },
-        status: :started,
-        turn: :player
-      }
+      expected_response = @start_game_player
 
       assert expected_response == Game.info()
 
-      new_state = %{
-        computer: %Player{
-          life: 50,
-          moves: %{
-            move_avg: :chute,
-            move_heal: :cura,
-            move_rnd: :soco
-            },
-          name: "Robotinik"
-        },
-        player: %Player{
-          life: 20,
-          moves: %{
-            move_avg: :chute,
-            move_heal: :cura,
-            move_rnd: :soco
-          },
-          name: "Jogador1"
-        },
-        status: :started,
-        turn: :player
-      }
+      player = %{@start_game_player.player | life: @life}
+      computer = %{@start_game_player.computer | life: @life}
+
+      new_state = %{@start_game_player | player: player, computer: computer}
 
       Game.update(new_state)
 
       expected_response = %{new_state | status: :continue, turn: :computer}
 
       assert expected_response == Game.info()
+    end
+  end
+
+  describe "player/0" do
+    test "returns the player of the game" do
+      Game.start(@computer, @player, :player)
+
+      expected_response = @start_game_player.player
+
+      assert expected_response == Game.player()
+    end
+  end
+
+  describe "turn/0" do
+    test "returns the turn of the game" do
+      Game.start(@computer, @player, :player)
+
+      expected_response = :player
+
+      assert expected_response == Game.turn()
+
+      new_state = @start_game_player
+
+      Game.update(new_state)
+
+      expected_response = :computer
+
+      assert expected_response == Game.turn()
+    end
+  end
+
+  describe "fetch_player/1" do
+    test "returns the player of the game" do
+      Game.start(@computer, @player, :computer)
+
+      expected_response = @start_game_player.computer
+
+      assert expected_response == Game.fetch_player(:computer)
     end
   end
 end
